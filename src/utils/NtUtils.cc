a9260a3a1ed220835a449e69c6170df0b08d6009
dabeycorn <Dasun20202020@gmail.com>
2026-02-11 19:55:16 -0500

    create util method for starting nt
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

---
+++ src/utils/NtUtils.cc
@@ -0,0 +1,24 @@
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+//
+// File: NtUtils.cc
+// Purpose: TODO
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+#include "src/utils/NtUtils.h"
+
+namespace utils {
+    void StartNetworkTables() {
+        nt::NetworkTableInstance instance = nt::NetworkTableInstance::GetDefault();
+
+        instance.StopClient();
+        instance.StopLocal();
+
+        instance.StartClient4("iron-sight");
+        instance.SetServerTeam(6423);
+
+        frc::DataLogManager::Start("/iron-sight/logs");
+        printf("Started NetworkTables");
+    }
+}
---
+++ src/utils/NtUtils.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+//
+// File: NtUtils.h
+// Purpose: utility type methods for interacting with NetworkTables
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+#pragma once
+#include "src/utils/PCH.h"
+
+namespace utils {
+    /** Start networktables */
+    void StartNetworkTables();
+}
