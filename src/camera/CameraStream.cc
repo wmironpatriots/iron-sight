00943b8ba315a9915808e287db31fdb906da64db
dabeycorn <Dasun20202020@gmail.com>
2026-02-12 20:07:08 -0500

    Create CameraStream
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

---
+++ src/camera/CameraStream.cc
@@ -0,0 +1,45 @@
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+//
+// File: CameraStream.cc
+// Purpose: Define 
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+
+#include "src/camera/CameraStream.h"
+#include <memory>
+#include <opencv2/core/mat.hpp>
+#include <thread>
+#include "src/camera/Camera.h"
+
+namespace camera {
+    CameraStream::CameraStream(std::unique_ptr<CameraIO> io) : mIO(std::move(io)) {
+        mTimestampedFrame = mIO->getTimestampedFrame();
+
+        mThread = std::thread([this] () -> void {
+            TimestampedFrame tframe;
+            tframe = mIO->getTimestampedFrame();
+
+            mMutex.lock();
+            mTimestampedFrame = tframe;
+            mMutex.unlock();
+        });
+    };
+
+    auto CameraStream::getTimestampedFrame() -> TimestampedFrame {
+        mMutex.lock();
+        TimestampedFrame tframe = mTimestampedFrame;
+        mMutex.unlock();
+
+        return tframe;
+    };
+
+    auto CameraStream::getFrame() -> cv::Mat {
+        mMutex.lock();
+        cv::Mat frame = mTimestampedFrame.frame;
+        mMutex.unlock();
+
+        return frame;
+    };
+}
---
+++ src/camera/CameraStream.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+//
+// File: CameraStream.h
+// Purpose: Define camera streams
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+#pragma once
+
+#include <memory>
+#include <mutex>
+#include <opencv2/core/mat.hpp>
+#include <thread>
+#include "src/camera/Camera.h"
+#include "src/utils/PCH.h"
+
+namespace camera {
+    /** Represents a continious stream of timestamped frames from camera */
+    class CameraStream {
+        public:
+            CameraStream(std::unique_ptr<CameraIO> IO);
+            /** Return lastest timestamped frame from stream */
+            auto getTimestampedFrame() -> TimestampedFrame;
+            /** Return latest raw frame */
+            auto getFrame() -> cv::Mat;
+        private:
+            std::unique_ptr<CameraIO> mIO;
+            TimestampedFrame mTimestampedFrame;
+            std::thread mThread;
+            std::mutex mMutex;
+    };
+}
