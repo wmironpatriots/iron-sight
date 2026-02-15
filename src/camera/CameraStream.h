de83a88b00ca95e6b95745fc4c9ed1ccfb6026ba
Dasun A. <Dasun20202020@hotmail.com>
2026-02-15 12:08:02 -0500

    Create Camera Lib
    * CMakeLists.txt update
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * Create precompiled headers file
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * create util method for starting nt
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * comment cleanup
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * Create CameraIO and OpenCV extension
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * Create CameraStream
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * update camera lib CMakeLists
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * remove uneccesary includes
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * rename opencv camera interface to CameraCv
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * added .vscode/ to gitignore
    
    Signed-off-by: bobz5460 <bzhang5460@gmail.com>
    
    ---------
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    Signed-off-by: bobz5460 <bzhang5460@gmail.com>
    Co-authored-by: bobz5460 <bzhang5460@gmail.com>

---
+++ src/camera/CameraStream.h
@@ -0,0 +1,28 @@
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+//
+// File: CameraStream.h
+// Purpose: Define camera streams
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+#pragma once
+
+#include "src/camera/Camera.h"
+
+namespace camera {
+    /** Represents a continious stream of timestamped frames from camera */
+    class CameraStream {
+        public:
+            CameraStream(std::unique_ptr<Camera> IO);
+            /** Return lastest timestamped frame from stream */
+            auto getTimestampedFrame() -> TimestampedFrame;
+            /** Return latest raw frame */
+            auto getFrame() -> cv::Mat;
+        private:
+            std::unique_ptr<Camera> mIO;
+            TimestampedFrame mTimestampedFrame;
+            std::thread mThread;
+            std::mutex mMutex;
+    };
+}
