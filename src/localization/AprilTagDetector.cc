c7176ec13591e6e08f3b2e2d8f81974e098b4f40
bobz5460 <bzhang5460@gmail.com>
2026-02-15 19:54:17 -0500

    wip apriltag detector
    Signed-off-by: bobz5460 <bzhang5460@gmail.com>

---
+++ src/localization/AprilTagDetector.cc
@@ -0,0 +1,1 @@
+#include "src/localization/AprilTagDetector.h"
--- src/localization/AprilTagDetector.h
+++ src/localization/AprilTagDetector.h
@@ -1,1 +1,10 @@
 #include <apriltag/apriltag.h>
+#include "src/utils/PCH.h"
+
+namespace localization {
+    class AprilTagDetector {
+        public:
+            virtual ~AprilTagDetector() = default;
+            virtual auto getDetectedTags() -> std::vector<apriltag_detection_t*>;
+    };
+}
