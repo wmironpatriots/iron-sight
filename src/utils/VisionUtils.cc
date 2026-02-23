0cbdfb9debbd4035b5e901a7862b0dd00d54c751
dabeycorn <Dasun20202020@gmail.com>
2026-02-22 20:52:07 -0500

    opencv vec to wpilib transform fix question mark
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/utils/VisionUtils.cc
+++ src/utils/VisionUtils.cc
@@ -4,17 +4,33 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // MIT license file in the root directory of this project
 #include "src/utils/VisionUtils.h"
+#include <frc/geometry/Rotation3d.h>
 
 namespace utils {
     auto ConvertOpencvRvecTvecToWpiLibTransform(cv::Mat rvec, cv::Mat tvec) -> frc::Transform3d {
-        Eigen::Vector3d T(tvec.at<double>(0), tvec.at<double>(1), tvec.at<double>(2));
+        Eigen::Vector3d T(
+            tvec.at<double>(0), 
+            tvec.at<double>(1), 
+            tvec.at<double>(2));
         auto translation = frc::Translation3d(T);
 
-        Eigen::Vector3d R(rvec.at<double>(0), rvec.at<double>(1), rvec.at<double>(2));
-        auto rotation = frc::Rotation3d(R, units::angle::radian_t{R.norm()});
+        Eigen::Vector3d R(
+            rvec.at<double>(0), 
+            rvec.at<double>(1), 
+            rvec.at<double>(2));
+        auto rotation = frc::Rotation3d();
 
-        auto cvFieldToCamera = frc::Transform3d(translation, rotation).Inverse();
-        auto wpilibFieldToCamera = frc::CoordinateSystem::Convert(cvFieldToCamera, frc::CoordinateSystem::EDN(), frc::CoordinateSystem::NWU());
+        if (R.norm() > 1e-6) {
+            rotation = frc::Rotation3d(R, units::angle::radian_t{R.norm()});
+        }
+
+        auto cvFieldToCamera = frc::Transform3d(
+            translation, 
+            rotation).Inverse();
+        auto wpilibFieldToCamera = frc::CoordinateSystem::Convert(
+            cvFieldToCamera, 
+            frc::CoordinateSystem::EDN(), 
+            frc::CoordinateSystem::NWU());
         
         return wpilibFieldToCamera;
     }
