1ddc27ffcdcb47e81f0a72f215563451af8707b1
dabeycorn <Dasun20202020@gmail.com>
2026-02-24 23:29:05 -0500

    change kTagCorners order to BR, BL, TL, TR
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/localization/PositionEstimatorIO.h
+++ src/localization/PositionEstimatorIO.h
@@ -17,12 +17,12 @@ namespace localization {
     /** Side Length of Tags */
     constexpr units::length::meter_t kTagSideLength{0.1651};
 
-    /** Coordinates of Tag Corners (TL, TR, BR, BL) */
+    /** Coordinates of Tag Corners (BR, BL, TL, TR) */
     const std::vector<frc::Pose3d> kTagCorners = {
-        frc::Pose3d(kTagSideLength / -2, kTagSideLength / 2, 0_m, frc::Rotation3d()),
-        frc::Pose3d(kTagSideLength / 2, kTagSideLength / 2, 0_m, frc::Rotation3d()),
-        frc::Pose3d(kTagSideLength / 2, kTagSideLength / -2, 0_m, frc::Rotation3d()),
-        frc::Pose3d(kTagSideLength / -2, kTagSideLength / -2, 0_m, frc::Rotation3d())
+        frc::Pose3d(kTagSideLength / 2, -kTagSideLength / 2, 0_m, frc::Rotation3d()),
+        frc::Pose3d(-kTagSideLength / 2, -kTagSideLength / 2, 0_m, frc::Rotation3d()),
+        frc::Pose3d(-kTagSideLength / 2, kTagSideLength / 2, 0_m, frc::Rotation3d()),
+        frc::Pose3d(kTagSideLength / 2, kTagSideLength / 2, 0_m, frc::Rotation3d())
     };
 
     /** An estimated position in 3-Dimensional space /w a confidence value */
