f68370102977c0d903a5e09577fd77e2fb4e8fee
Frank Wang <frankwangli0326@gmail.com>
2026-02-27 22:05:41 -0500

    create gamepiece calculation classes
    Signed-off-by: Frank Wang <frankwangli0326@gmail.com>

---
+++ src/gamepiece/gamepiece.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <frc/geometry/Pose2d.h>
+#include <networktables/StructTopic.h>
+#include <nlohmann/json.hpp>
+#include <vector>
+#include "src/camera/CameraStream.h" 
+#include "src/camera/CameraConfig.h"
+#include "src/utils/PCH.h"
+#include "src/yolo/Yolo.h"
+
+namespace gamepiece {
+
+// 2026: single gamepiece "fuel"
+void run_fuel_detect(yolo::Yolo& model,
+                     const std::vector<std::string>& class_names,
+                     camera::camera_config_t config,
+                     nt::StructTopic<frc::Pose2d>& fuel_topic,
+                     bool debug);
+
+void run_fuel_detect_no_img(yolo::Yolo& model,
+                            const std::vector<std::string>& class_names);
+
+}  // namespace gamepiece
