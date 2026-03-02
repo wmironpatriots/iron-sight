28f5ffe487108e0fe13a7870b555aed5dcfe2339
dabeycorn <Dasun20202020@gmail.com>
2026-03-01 20:27:11 -0500

    cleanup u1
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/u1_main.cc
+++ src/u1_main.cc
@@ -1,3 +1,7 @@
+#include <frc/geometry/Rotation3d.h>
+#include <wpimath/frc/geometry/Transform3d.h>
+#include <cstdio>
+#include <thread>
 #include "src/localization/PositionEstimatePublisher.h"
 #include "src/localization/TagSearcherIOAruco.h"
 #include "src/localization/TagSearcherIOWpiLib.h"
@@ -7,9 +11,17 @@
 #include "src/camera/CameraIOCv.h"
 #include "src/localization/PositionEstimatorIOMultiTag.h"
 #include "src/localization/PositionEstimatorIOSingleTag.h"
-#include <iostream>
+#include <units/length.h>
 
-inline const camera::camera_config_t kDemoCam = camera::camera_config_t{
+/**
+    A Script for the Orange Pi Unit connected to the Front and Back
+    Camera of FRC 6423's 2026 Robot
+*/
+
+inline const frc::AprilTagFieldLayout kFieldLayout = frc::AprilTagFieldLayout::LoadField(frc::AprilTagField::k2026RebuiltWelded);
+
+// TODO intrinsics
+inline const camera::camera_config_t kBessieConfig = camera::camera_config_t{
     "bessie",
     0,
     cv::CAP_V4L2,
@@ -17,45 +29,91 @@ inline const camera::camera_config_t kDemoCam = camera::camera_config_t{
     1280,
     800,
     120,
-    frc::Transform3d(),
+    frc::Transform3d(
+        0.0_in, 
+        0.0_in, 
+        0.0_in, 
+        frc::Rotation3d(
+            0.0_rad,
+            0.0_rad,
+            0.0_rad
+        )
+    ),
     camera::camera_intrinsics_t{
-        619.9026951017695,
-        372.2792812903024,
-        539.3898006061588,
-        539.0735059998198,
-        -0.3241468189388152,
-        0.10782527225392564,
-        -0.016410664585260946,
-        0.00013396458313481827,
-        -0.0002794187083645791
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
     }
 };
-/** Represents the program for the center cams unit */
-auto main() -> int {
-    camera::CameraIOCv camera(kDemoCam);
-
-    auto searcher = localization::TagSearcherIOWpiLib();
-    const frc::AprilTagFieldLayout fieldLayout = frc::AprilTagFieldLayout::LoadField(frc::AprilTagField::k2026RebuiltAndyMark);
-
-    auto poseEstimator = localization::PositionEstimatorIOMultiTag(fieldLayout, kDemoCam);
-    auto squarePoseEstimator = localization::PositionEstimatorIOSingleTag(fieldLayout, kDemoCam);
-
-    utils::StartNetworkTables(true);
-    auto publisher = localization::PositionEstimatePublisher(kDemoCam);
-    
-    while (true){
-        camera::timestamped_frame_t tframe = camera.GetTimestampedFrame();
-        
-        auto detections = searcher.FindTagsFromTimestampedFrame(tframe);
-        //TODO add check for how many apriltags are seen    
-
-        auto pose = poseEstimator.Estimate3dPoseFromFoundTags(detections);
-        auto squarepose = squarePoseEstimator.Estimate3dPoseFromFoundTags(detections);
-        
-        //TODO cache poses to publisher to not overwrite here, also differentiate from single/multi in one publisher
-        if (!squarepose.empty()) publisher.Publish(squarepose[0]);
-        if (!pose.empty()) publisher.Publish(pose[0]);
-
+// TODO extrinsics & intrinsics
+inline const camera::camera_config_t kElsieConfig = camera::camera_config_t{
+    "elsie",
+    1,
+    cv::CAP_V4L2,
+    "MJPG",
+    1280,
+    800,
+    120,
+    frc::Transform3d(
+        0.0_in, 
+        0.0_in, 
+        0.0_in, 
+        frc::Rotation3d(
+            0.0_rad,
+            0.0_rad,
+            0.0_rad
+        )
+    ),
+    camera::camera_intrinsics_t{
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
     }
-    return 0;
+};
+
+/** Entry Point */
+auto main() -> int {
+    utils::StartNetworkTables(false);
+
+    /* ~ CAMERA INIT ~ */
+    std::printf("Initializing Front Camera (Bessie)");
+    camera::CameraIOCv camera(kBessieConfig);
+
+    // TODO Elsie Init
+
+    /* ~ SEARCHER/ESTIMATOR INIT ~ */
+    auto searcher = localization::TagSearcherIOWpiLib();
+
+    auto poseEstimator = localization::PositionEstimatorIOMultiTag(kFieldLayout, kBessieConfig);
+    auto squarePoseEstimator = localization::PositionEstimatorIOSingleTag(kFieldLayout, kBessieConfig);
+
+    /* ~ PUBLISHER INIT ~ */
+    auto publisher = localization::PositionEstimatePublisher(kBessieConfig);
+    
+    /* ~ THREAD INIT ~ */
+    std::thread front_thread([&camera, &searcher, &poseEstimator, &squarePoseEstimator, &publisher] () -> void {
+        while (true) {
+            camera::timestamped_frame_t tframe = camera.GetTimestampedFrame();
+            
+            auto detections = searcher.FindTagsFromTimestampedFrame(tframe);
+
+            auto pose = poseEstimator.Estimate3dPoseFromFoundTags(detections);
+            auto squarepose = squarePoseEstimator.Estimate3dPoseFromFoundTags(detections);
+
+            if (squarepose.size() == 1) publisher.Publish(squarepose[0]);
+            if (!pose.empty()) publisher.Publish(pose[0]);
+        }
+    });
 }
